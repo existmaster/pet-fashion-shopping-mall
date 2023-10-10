c170b227f043df61668058872bdaca9b7f3fe74a
Sung-Yeol Yun <existmaster@gmail.com>
2023-10-11 00:43:14 +0900

    commit

---
+++ frontend-wijmo/src/components/PetInformationGrid.vue
@@ -0,0 +1,52 @@
+<template>
+    <div>
+        <v-data-table
+                :headers="headers"
+                :items="value"
+                :items-per-page="5"
+                class="elevation-1"
+        ></v-data-table>
+    </div>
+</template>
+
+<script>
+    import BaseUIGrid from './base-ui/BaseUIGrid';
+
+    export default {
+        mixins: [BaseUIGrid],
+        name: 'PetInformation-PetInformation',
+        data: () => ({
+            headers: [
+                {
+                    text: 'id',
+                    value: 'id'
+                },
+                {
+                    text: 'memberId',
+                    value: 'memberId'
+                },
+                {
+                    text: 'allergies',
+                    value: 'allergies'
+                },
+                {
+                    text: 'neckCircumference',
+                    value: 'neckCircumference'
+                },
+                {
+                    text: 'chestCircumference',
+                    value: 'chestCircumference'
+                },
+                {
+                    text: 'Allergies',
+                    value: 'Allergies'
+                },
+            ],
+        }),
+        async created() {
+            const path = "petInformations"
+            await this.getValue(path);
+        },
+    }
+</script>
+
